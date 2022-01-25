f473e9ba43a902612c9d4de96f63e4ad4471341f
azuryte5 <85147307+azuryte5@users.noreply.github.com>
2022-01-25 11:48:51 -0500

    react color example added, concurrently added

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import React, { useState }  from "react";
 import './App.css';
+import { GithubPicker } from 'react-color';
 
 function App() {
+  const [currentColour, setColour] = useState("#B80000");
+  console.log(currentColour)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+      <h1 >
+      <p style={{color:currentColour}}>Colour</p>
+      <p style={{color:currentColour}}>Harmony</p>
+      <p style={{color:currentColour}}>Blend</p>
+      </h1>
       </header>
+      <div>
+      <GithubPicker 
+      color={currentColour}
+      onChangeComplete={color => {setColour(color.hex)}}
+      width="200px" 
+      colors={['#B80000', '#DB3E00', '#FCCB00', '#008B02', '#006B76', '#1273DE', '#004DCF', '#5300EB', '#EB9694', '#FAD0C3', '#FEF3BD', '#C1E1C5', '#BEDADC', '#C4DEF6', '#BED3F3', '#D4C4FB']}
+      triangle="hide" 
+      // onSwatchHover
+      />
+      </div>
     </div>
   );
 }
