d2d78a748f69db404e12e4bdd88dffa6b8c3ae1b
azuryte5 <85147307+azuryte5@users.noreply.github.com>
2022-01-27 09:40:19 -0500

    Bug with multiple colouring fields at once fixed

--- client/src/components/House/index.js
+++ client/src/components/House/index.js
@@ -13,11 +13,11 @@ class House extends React.Component {
       >
         <g id="Layer 1">
           <path
-          onClick={() => this.props.onFill(0)}
+          onClick={() => this.props.onFill(17)}
             strokeWidth="43.4883"
             stroke="#000000"
             strokeLinejoin="round"
-            fill={this.props.fillColours[0]} 
+            fill={this.props.fillColours[17]} 
             strokeLinecap="round"
             d="M1209.28+336.654L2170.32+1022.76L1814.77+2148.78L633.986+2158.59L259.772+1038.64L1209.28+336.654Z"
             opacity="1"
@@ -32,29 +32,29 @@ class House extends React.Component {
             strokeLinejoin="round"
           />
           <path
-          onClick={() => this.props.onFill(1)}
+          onClick={() => this.props.onFill(18)}
             strokeWidth="43.4883"
             stroke="#000000"
             opacity="1"
             d="M830.282+1637.51L1078.87+1637.51C1079.31+1637.51+1079.67+1637.81+1079.67+1638.18L1079.67+2160.75C1079.67+2161.12+1079.31+2161.42+1078.87+2161.42L830.282+2161.42C829.839+2161.42+829.481+2161.12+829.481+2160.75L829.481+1638.18C829.481+1637.81+829.839+1637.51+830.282+1637.51Z"
-            fill={this.props.fillColours[1]} 
+            fill={this.props.fillColours[18]} 
             strokeLinecap="round"
             strokeLinejoin="round"
           />
           <path
-          onClick={() => this.props.onFill(2)}
+          onClick={() => this.props.onFill(18)}
             strokeLinecap="round"
             strokeWidth="43.4883"
-            fill={this.props.fillColours[2]} 
+            fill={this.props.fillColours[18]} 
             opacity="1"
             d="M1372.48+1531.32L1647.1+1531.32C1647.41+1531.32+1647.67+1531.57+1647.67+1531.89L1647.67+1904.23C1647.67+1904.54+1647.41+1904.8+1647.1+1904.8L1372.48+1904.8C1372.17+1904.8+1371.91+1904.54+1371.91+1904.23L1371.91+1531.89C1371.91+1531.57+1372.17+1531.32+1372.48+1531.32Z"
             strokeLinejoin="round"
             stroke="#000000"
           />
           <path
-          onClick={() => this.props.onFill(3)}
+          onClick={() => this.props.onFill(19)}
             strokeWidth="43.4883"
-            fill={this.props.fillColours[3]} 
+            fill={this.props.fillColours[19]} 
             strokeLinejoin="round"
             strokeLinecap="round"
             stroke="#000000"
@@ -62,9 +62,9 @@ class House extends React.Component {
             d="M290.662+1036.99L2145.77+1036.99C2147.47+1036.99+2148.84+1038.36+2148.84+1040.06L2148.84+1099.6C2148.84+1101.3+2147.47+1102.67+2145.77+1102.67L290.662+1102.67C288.967+1102.67+287.592+1101.3+287.592+1099.6L287.592+1040.06C287.592+1038.36+288.967+1036.99+290.662+1036.99Z"
           />
           <path
-          onClick={() => this.props.onFill(4)}
+          onClick={() => this.props.onFill(20)}
             strokeWidth="43.4883"
-            fill={this.props.fillColours[4]} 
+            fill={this.props.fillColours[20]} 
             strokeLinecap="round"
             d="M713.299+1257.1L1136.08+1257.1C1137.78+1257.1+1139.15+1258.47+1139.15+1260.17L1139.15+1467.6C1139.15+1469.29+1137.78+1470.67+1136.08+1470.67L713.299+1470.67C711.603+1470.67+710.229+1469.29+710.229+1467.6L710.229+1260.17C710.229+1258.47+711.603+1257.1+713.299+1257.1Z"
             stroke="#000000"
@@ -72,29 +72,29 @@ class House extends React.Component {
             opacity="1"
           />
           <path
-          onClick={() => this.props.onFill(5)}
+          onClick={() => this.props.onFill(21)}
             strokeWidth="43.4883"
             strokeLinecap="round"
             d="M2561.34+587.664C2561.34+402.849+2714.89+253.026+2904.29+253.026C3093.7+253.026+3247.25+402.849+3247.25+587.664C3247.25+772.48+3093.7+922.302+2904.29+922.302C2714.89+922.302+2561.34+772.48+2561.34+587.664Z"
-            fill={this.props.fillColours[5]} 
+            fill={this.props.fillColours[21]} 
             opacity="1"
             strokeLinejoin="round"
             stroke="#000000"
           />
           <path
-          onClick={() => this.props.onFill(6)}
+          onClick={() => this.props.onFill(22)}
             d="M963.298+1912.68C963.298+1891.95+981.039+1875.14+1002.92+1875.14C1024.81+1875.14+1042.55+1891.95+1042.55+1912.68C1042.55+1933.41+1024.81+1950.22+1002.92+1950.22C981.039+1950.22+963.298+1933.41+963.298+1912.68Z"
             stroke="#000000"
             strokeWidth="20"
             strokeLinecap="round"
             opacity="1"
             strokeLinejoin="round"
-            fill={this.props.fillColours[6]} 
+            fill={this.props.fillColours[22]} 
           />
           <path
-          onClick={() => this.props.onFill(7)}
+          onClick={() => this.props.onFill(23)}
             strokeWidth="43.4883"
-            fill={this.props.fillColours[7]} 
+            fill={this.props.fillColours[23]} 
             stroke="#000000"
             d="M1109.28+748.961C1109.28+684.46+1158.77+632.172+1219.82+632.172C1280.86+632.172+1330.35+684.46+1330.35+748.961C1330.35+813.462+1280.86+865.75+1219.82+865.75C1158.77+865.75+1109.28+813.462+1109.28+748.961Z"
             strokeLinejoin="round"
@@ -102,11 +102,11 @@ class House extends React.Component {
             opacity="1"
           />
           <path
-          onClick={() => this.props.onFill(8)}
+          onClick={() => this.props.onFill(24)}
             strokeLinejoin="round"
             opacity="1"
             d="M90.3157+2179.62L3300.31+2179.62C3301.93+2179.62+3303.24+2181.87+3303.24+2184.64L3303.24+2290.42C3303.24+2293.19+3301.93+2295.44+3300.31+2295.44L90.3157+2295.44C88.6958+2295.44+87.3826+2293.19+87.3826+2290.42L87.3826+2184.64C87.3826+2181.87+88.6958+2179.62+90.3157+2179.62Z"
-            fill={this.props.fillColours[8]} 
+            fill={this.props.fillColours[24]} 
             stroke="#000000"
             strokeLinecap="round"
             strokeWidth="43.4883"
