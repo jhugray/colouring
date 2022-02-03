57ec74e08bbd3bea4d17af0b7232050238d8528d
Jess Hause Ugray <jess.ugray@gmail.com>
2022-02-03 06:47:51 -0500

    server updates

--- server/server.js
+++ server/server.js
@@ -26,21 +26,19 @@ app.use(express.json());
 
 
 // if we're in production, serve client/build as static assets
+// if (process.env.NODE_ENV === 'production') {
+//   app.use((req, res, next) => {
+//     if (req.header('x-forwarded-proto') !== 'https')
+//       res.redirect(`https://${req.header('host')}${req.url}`)
+//     else
+//       next()
+//   })
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
+
 if (process.env.NODE_ENV === 'production') {
-  app.use((req, res, next) => {
-    if (req.url === "/") {
-      res.redirect(`https://${req.header('host')}/colouring`)
-    }
-    else if (req.header('x-forwarded-proto') !== 'https') {
-      res.redirect(`https://${req.header('host')}${req.url}`)
-    }
-    else {
-      next()
-    }
-  })
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
-
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
